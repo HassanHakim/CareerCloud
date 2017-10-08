15cca5e2781d1eeb18dde0a17fe0b4342910b2c1
HassanHakim <Hassan.a.mostafa@gmail.com>
2017-10-08 07:15:07 -0400

    WebAPI project added

---
+++ CareerCloud.WebAPI/Controllers/CompanyJobDescriptionController.cs
@@ -0,0 +1,76 @@
+﻿using CareerCloud.ADODataAccessLayer;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [RoutePrefix("api/careercloud/company/v1")]
+    public class CompanyJobDescriptionController : ApiController
+    {
+        private CompanyJobDescriptionLogic _logic;
+        public CompanyJobDescriptionController()
+        {
+            var repo = new EFGenericRepository<CompanyJobDescriptionPoco>(false);
+            _logic = new CompanyJobDescriptionLogic(repo);
+        }
+        [HttpGet]
+        [Route("JobDescription/{CompanyJobDescriptionId}")]
+        [ResponseType(typeof(CompanyJobDescriptionPoco))]
+        public IHttpActionResult GetCompanyJobDescription(Guid CompanyJobDescriptionId)
+        {
+            CompanyJobDescriptionPoco companyJobDescription = _logic.Get(CompanyJobDescriptionId);
+            if (companyJobDescription == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyJobDescription);
+        }
+
+        [HttpGet]
+        [Route("JobDescription")]
+        [ResponseType(typeof(List<CompanyJobDescriptionPoco>))]
+        public IHttpActionResult GetAllCompanyJobDescription()
+        {
+            var companyJobDescriptionList = _logic.GetAll();
+            if (companyJobDescriptionList == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyJobDescriptionList);
+        }
+
+
+        [HttpPost]
+        [Route("JobDescription")]
+        public IHttpActionResult PostCompanyJobDescription([FromBody] CompanyJobDescriptionPoco[] CompanyJobDescriptions)
+        {
+            _logic.Add(CompanyJobDescriptions);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("JobDescription")]
+        public IHttpActionResult PutCompanyJobDescription([FromBody] CompanyJobDescriptionPoco[] CompanyJobDescriptions)
+        {
+            _logic.Update(CompanyJobDescriptions);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("JobDescription")]
+        public IHttpActionResult DeleteCompanyJobDescription([FromBody] CompanyJobDescriptionPoco[] CompanyJobDescriptions)
+        {
+            _logic.Delete(CompanyJobDescriptions);
+            return Ok();
+        }
+    }
+}
---
+++ CareerCloud.WebAPI/Controllers/SecurityLoginsLogController.cs
@@ -0,0 +1,76 @@
+﻿using CareerCloud.ADODataAccessLayer;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [RoutePrefix("api/careercloud/security/v1")]
+    public class SecurityLoginsLogController : ApiController
+    {
+        private SecurityLoginsLogLogic _logic;
+        public SecurityLoginsLogController()
+        {
+            var repo = new EFGenericRepository<SecurityLoginsLogPoco>(false);
+            _logic = new SecurityLoginsLogLogic(repo);
+        }
+        [HttpGet]
+        [Route("LoginsLog/{SecurityLoginsLogId}")]
+        [ResponseType(typeof(SecurityLoginsLogPoco))]
+        public IHttpActionResult GetSecurityLoginsLog(Guid SecurityLoginsLogId)
+        {
+            SecurityLoginsLogPoco securityLoginsLog = _logic.Get(SecurityLoginsLogId);
+            if (securityLoginsLog == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLoginsLog);
+        }
+
+        [HttpGet]
+        [Route("LoginsLog")]
+        [ResponseType(typeof(List<SecurityLoginsLogPoco>))]
+        public IHttpActionResult GetAllSecurityLoginsLog()
+        {
+            var securityLoginsLogList = _logic.GetAll();
+            if (securityLoginsLogList == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLoginsLogList);
+        }
+
+
+        [HttpPost]
+        [Route("LoginsLog")]
+        public IHttpActionResult PostSecurityLoginsLog([FromBody] SecurityLoginsLogPoco[] SecurityLoginsLogs)
+        {
+            _logic.Add(SecurityLoginsLogs);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("LoginsLog")]
+        public IHttpActionResult PutSecurityLoginsLog([FromBody] SecurityLoginsLogPoco[] SecurityLoginsLogs)
+        {
+            _logic.Update(SecurityLoginsLogs);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("LoginsLog")]
+        public IHttpActionResult DeleteSecurityLoginsLog([FromBody] SecurityLoginsLogPoco[] SecurityLoginsLogs)
+        {
+            _logic.Update(SecurityLoginsLogs);
+            return Ok();
+        }
+    }
+}
